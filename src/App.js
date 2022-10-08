ef41833b2d60f29ca27988f9fc280a9225dd5daf
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-10-08 22:23:51 +0200

    route settings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import './App.scss';
-
+import "./App.scss";
+import React from "react";
+import {  Switch, Route } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+        <Header />
+        <Switch>
+          <Route exact  path="/"  component={Home}/>
+          <Route path="/movie/:imdbID" component={MovieDetail}/>
+          <Route component={PageNotFound} />
+        </Switch>
+        <Footer />
     </div>
   );
 }
