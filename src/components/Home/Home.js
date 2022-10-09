994ac437cb393e643456e3333a0de9209edfbc06
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-10-09 13:23:23 +0200

    adding redux dispatch added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,24 @@
-import React from "react";
-import MovieListing from "../MovieListing/MovieListing"
+import React, { useEffect, useState } from "react";
+import MovieListing from "../MovieListing/MovieListing";
 import "./Home.scss";
+import movieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 const Home = () => {
+  const dispatch=useDispatch()
+  useEffect(() => {
+    const movieText = "Harry";
+   
+    const fetchMovies = async () => {
+      const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`).catch((e) => {
+        console.log("Error:",e);
+      });
+      // console.log("Response from api:",response);
+      dispatch(addMovies(response.data))
+    };
+      fetchMovies();
+  }, []);
   return (
     <div>
       <div className="banner-img"> </div>
