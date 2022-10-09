238d75694c0c30df02123e7f39d3ffeda68febf9
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-10-09 13:51:52 +0200

    moviecard created

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
-const MovieCard = () => {
+import "./MovieCard.scss"
+const MovieCard = (props) => {
+    const {data}=props
     return (
-        <div>
-            card
+        <div className='card-item'>
+            <div className="card-inner">
+                <div className="card-top">
+                    <img src={data.Poster} alt={data.Title} />
+                </div>
+                <div className="card-bottom">
+                    <div className="card-info">
+                        <h4>{data.Title}</h4>
+                        <p>{data.Year}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
