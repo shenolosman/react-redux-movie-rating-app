6a09dec4dd5515706091325bd623aee009225789
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-10-10 23:28:44 +0200

    slider trying to add

--- README react.md
+++ README react.md
@@ -2,7 +2,9 @@
 
 ### fetching data from `https://www.omdbapi.com/`
 
-installed : `npx create-react-app ./`, `npm install node-sass --save` , `npm install axios` , `npm  install react-router-dom@5` , `npm install redux react-redux @reduxjs/toolkit`
+installed : `npx create-react-app ./`, `npm install node-sass --save` , `npm install axios` , `npm  install react-router-dom@5` , `npm install redux react-redux @reduxjs/toolkit` for carusel `npm install react-slick`
 
 
 nmp start  `http://localhost:3000`
+
+
--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -3,6 +3,8 @@ import { useSelector } from "react-redux";
 import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
 import MovieCard from "../MovieCard/MovieCard";
 import "./MovieListing.scss";
+// import Slider  from "react-slick";
+
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
   const shows = useSelector(getAllShows);
@@ -25,11 +27,24 @@ const MovieListing = () => {
         <h3>{shows.Error}</h3>
       </div>
     );
+
+  // const settings = {
+  //   dots: false,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 6,
+  //   slidesToScroll: 3,
+  // };
+
   return (
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
-        <div className="movie-container">{renderMovies}</div>
+        <div className="movie-container">
+           {/* <Slider {...settings}> */}
+                {renderMovies}
+                {/* </Slider> */}
+    </div>
       </div>
       <div className="show-list">
         <h2>Shows</h2>
